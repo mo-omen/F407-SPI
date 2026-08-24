05abada1901f97c53c0f4413f889c37caf4f6f81
Momen Elhag <Mamoony2006@gmail.com>
2026-08-24 22:09:36 +0800

    Added: SPI_v1

---
+++ SPI_v1/Src/main.c
@@ -0,0 +1,55 @@
+#include <stdint.h>
+
+volatile uint32_t *RCC_AHB1ENR = (uint32_t *)0x40023830;
+volatile uint32_t *RCC_APB2ENR = (uint32_t *)0x40023844;
+
+volatile uint32_t *GPIOA_MODER = (uint32_t *)0x40020000;
+volatile uint32_t *GPIOA_AFRL = (uint32_t *)0x40020020;
+volatile uint32_t *GPIOA_ODR = (uint32_t *)0x40020014;
+
+volatile uint32_t *SPI1_CR1 = (uint32_t *)0x40013000;
+volatile uint32_t *SPI1_SR = (uint32_t *)0x40013008;
+volatile uint8_t *SPI1_DR = (uint8_t *)0x4001300C; // has to be 8 bit to match SPI ADXL345 frame
+
+int main(void) {
+
+	*RCC_AHB1ENR |= (1 << 0);
+	*RCC_APB2ENR |= (1 << 12);
+
+	*GPIOA_MODER &= ~(0xFF << 8);
+	*GPIOA_MODER |= ((1 << 8) | (1 << 11) | (1 << 13) | (1 << 15));
+	*GPIOA_AFRL &= ~(0xFFF << 20);
+	*GPIOA_AFRL |= ((5 << 20) | (5 << 24) | (5 << 28));
+	*GPIOA_ODR |= (1 << 4);
+
+	*SPI1_CR1 |= (1 << 3);	// Clock Speed divider '4'
+	*SPI1_CR1 |= (1 << 1);	// CLK polarity
+	*SPI1_CR1 |= (1 << 0);	// CLK phase
+	*SPI1_CR1 &= ~(1 << 11);	// 8-bit frame selection
+	*SPI1_CR1 |= (1 << 8);	// internal slave select
+	*SPI1_CR1 |= (1 << 9);	// Software slave management
+	*SPI1_CR1 |= (1 << 2);	// Master Config
+	*SPI1_CR1 |= (1 << 6);	// SPI enable
+
+	int buffer;
+	int DEVID;
+
+	while(1) {
+		*GPIOA_ODR &= ~(1 << 4);
+
+		while(!(*SPI1_SR & (1 << 1))) { }	// wait for TXE
+		*SPI1_DR = 0b10000000;				// send command byte
+
+		while(!(*SPI1_SR & (1 << 0))) { }	// wait for RXNE
+		buffer = *SPI1_DR;					// read byte [garbage init data]
+
+		while(!(*SPI1_SR & (1 << 1))) { }	// wait for TXE
+		*SPI1_DR = 0b00000000;				// write a dummy byte to push
+
+		while(!(*SPI1_SR & (1 << 0))) { }	// wait for RXNE
+		DEVID = *SPI1_DR;					// read sensor data
+
+		*GPIOA_ODR |= (1 << 4);
+	}
+
+}
